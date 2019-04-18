01cff5a619beb4c2b1c15432bf1c3bc7867fb5ef
deeksonparlma <dicksonparlma@gmail.com>
2019-04-18 14:33:32 +0300

    added classes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,23 @@
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
 import static spark.Spark.staticFileLocation;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+
+        //routes//
+        String layout = "public/templates/layout.vtl";
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "public/templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
     }
 }
