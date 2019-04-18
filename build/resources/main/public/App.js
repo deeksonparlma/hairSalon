4d085b70e68d39f7673c4e9ec509fe54c5c3f47b
deeksonparlma <dicksonparlma@gmail.com>
2019-04-18 16:42:56 +0300

    added content to the templates

---
+++ build/resources/main/public/App.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $("#danger").click(function(){
+    $("form").slideToggle(800);
+  });
+
+  }
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,7 @@ import spark.template.velocity.VelocityTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
 
+
 import static spark.Spark.get;
 import static spark.Spark.staticFileLocation;
 
@@ -14,9 +15,25 @@ public class App {
         //routes//
         String layout = "public/templates/layout.vtl";
 
+
+        //index file//
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             model.put("template", "public/templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        //admin or owner//
+        get("/admin", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "public/templates/admin.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        //stylist//
+        get("/stylists", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "public/templates/stylists.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
     }
