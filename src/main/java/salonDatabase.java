86488dd17c62e17edc374119cd58bef50f9d6bb1
deeksonparlma <dicksonparlma@gmail.com>
2019-04-25 11:54:22 +0300

    added the psql to the remote database at heroku

--- src/main/java/salonDatabase.java
+++ src/main/java/salonDatabase.java
@@ -1,5 +1,25 @@
-import org.sql2o.Sql2o;
-
+import org.sql2o.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 public class salonDatabase {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/salon", "postgres", "password");
+//    ?public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/salon", "postgres", "password");
+private static URI dbUri;
+    public static Sql2o sql2o;
+    static {
+
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://postgres:password@localhost:5432/salon");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[1];
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e ) {
+        }
+    }
 }
